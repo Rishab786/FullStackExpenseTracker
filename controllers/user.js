a527281428d17b4cfb44affff7b504627ebfe635
Rishab786 <147687802+Rishab786@users.noreply.github.com>
2023-12-12 19:11:33 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const path = require("path");
 const jwt = require("jsonwebtoken");
-const secretKey = "rishab@5$";
+const secretKey = "secretkey";
 
 exports.getUserDashboard = (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "addExpense.html"));
