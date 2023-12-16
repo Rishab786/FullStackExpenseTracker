3addecc76aa3ec7e3849ff898b846d576658b3bd
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-12-16 16:27:58 +0530

    BackendReady

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 const dotenv = require('dotenv');
 dotenv.config();
-const sequelize = new Sequelize("userDb","root", process.env.DB_PASS, {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 module.exports = sequelize;
